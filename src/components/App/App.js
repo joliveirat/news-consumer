376c992edecd2d6e958968a26423e1deebd55a13
Jéssica Teixeira <joliveirat@live.com>
2021-04-08 21:29:47 -0300

    feat: create paragraph component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import Button from "../Button";
 import Heading from "../Heading";
 import Subtitle from "../Subtitle";
+import Paragraph from "../Paragraph";
 
 import { GlobalStyle } from "./App.styles";
 
@@ -9,6 +10,15 @@ const App = () => (
     <GlobalStyle />
     <Heading type="h4">News Consumer</Heading>
     <Subtitle>Subtitle</Subtitle>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et mollis
+      nisl, finibus vestibulum tellus. Nulla volutpat ac tellus et sodales.
+      Donec vel interdum massa. Phasellus mollis eros a porta laoreet. Orci
+      varius natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Integer placerat venenatis ultricies. Suspendisse sed
+      ligula nunc. Sed feugiat faucibus iaculis. Suspendisse sagittis blandit
+      justo, ac tempor nibh imperdiet in.
+    </Paragraph>
     <Button color="primary">Button</Button>
   </>
 );
---
+++ src/components/Paragraph/Paragraph.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import * as tokens from "../../style/variables";
+
+export const Paragraph = styled.p`
+  margin: 0;
+  font-size: ${tokens.fontSizeXs};
+  font-weight: ${tokens.fontWeightRegular};
+  line-height: ${tokens.lineHeightDistant};
+  color: ${tokens.neutralColor2};
+`;
